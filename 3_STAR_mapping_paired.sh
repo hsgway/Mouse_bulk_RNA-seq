d2bdc1ec3ef76e213b245563c74128da6b13f0f9
hsgway <101917942+hsgway@users.noreply.github.com>
2024-01-08 23:52:16 -0500

    Update 3_STAR_mapping_paired.sh

--- 3_STAR_mapping_paired.sh
+++ 3_STAR_mapping_paired.sh
@@ -2,11 +2,11 @@
 #SBATCH -c 8                               # Request eight core
 #SBATCH -t 0-24:00                         # Runtime in D-HH:MM format
 #SBATCH -p priority                        # Partition to run in
-#SBATCH --mem-per-cpu=16G                 # Memory per core
+#SBATCH --mem-per-cpu=8G                   # Memory per core
 #SBATCH -o hostname_%j.out                 # File to which STDOUT will be written, including job ID (%j)
 #SBATCH -e hostname_%j.err                 # File to which STDERR will be written, including job ID (%j)
                                            # You can change the filenames given with -o and -e to any filenames you'd like
-#SBATCH --mail-user=yhsgw.ra@gmail.com
+#SBATCH --mail-user=xxx@bwh.harvard.edu
 #SBATCH --mail-type=ALL
 
 # load module
