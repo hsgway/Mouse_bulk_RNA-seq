a27ec059cc6a3357f1b9d144e3f49aa6c9a2b275
hsgway <101917942+hsgway@users.noreply.github.com>
2024-01-08 23:54:46 -0500

    Update 3_STAR_mapping_single.sh

--- 3_STAR_mapping_single.sh
+++ 3_STAR_mapping_single.sh
@@ -6,7 +6,7 @@
 #SBATCH -o hostname_%j.out                 # File to which STDOUT will be written, including job ID (%j)
 #SBATCH -e hostname_%j.err                 # File to which STDERR will be written, including job ID (%j)
                                            # You can change the filenames given with -o and -e to any filenames you'd like
-#SBATCH --mail-user=xxx@gbwh.harvard.edu
+#SBATCH --mail-user=xxx@bwh.harvard.edu
 #SBATCH --mail-type=ALL
 
 # load module
