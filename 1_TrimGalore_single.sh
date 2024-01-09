5bd52d81d9ebb3c57553fdbbfd9a27303c352f94
hsgway <101917942+hsgway@users.noreply.github.com>
2024-01-08 23:49:42 -0500

    Update 1_TrimGalore_single.sh

--- 1_TrimGalore_single.sh
+++ 1_TrimGalore_single.sh
@@ -2,11 +2,11 @@
 #SBATCH -c 8                               # Request eight core
 #SBATCH -t 0-04:00                         # Runtime in D-HH:MM format
 #SBATCH -p priority                        # Partition to run in
-#SBATCH --mem-per-cpu=16G                 # Memory per core
-#SBATCH -o /n/scratch3/users/s/sk608/hostname_%j.out                 # File to which STDOUT will be written, including job ID (%j)
-#SBATCH -e /n/scratch3/users/s/sk608/hostname_%j.err                 # File to which STDERR will be written, including job ID (%j)
+#SBATCH --mem-per-cpu=8G                 # Memory per core
+#SBATCH -o hostname_%j.out                 # File to which STDOUT will be written, including job ID (%j)
+#SBATCH -e hostname_%j.err                 # File to which STDERR will be written, including job ID (%j)
                                            # You can change the filenames given with -o and -e to any filenames you'd like
-#SBATCH --mail-user=yhsgw.ra@gmail.com
+#SBATCH --mail-user=xxx@bwh.harvard.edu    # your email address for receiving notification
 #SBATCH --mail-type=ALL
 
 # directory
