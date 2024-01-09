660a3d9095f89785cc0d67bb3dd8de2793f6179e
hsgway <101917942+hsgway@users.noreply.github.com>
2024-01-08 23:52:31 -0500

    Update 1_TrimGalore_single.sh

--- 1_TrimGalore_single.sh
+++ 1_TrimGalore_single.sh
@@ -2,7 +2,7 @@
 #SBATCH -c 8                               # Request eight core
 #SBATCH -t 0-24:00                         # Runtime in D-HH:MM format
 #SBATCH -p priority                        # Partition to run in
-#SBATCH --mem-per-cpu=8G                 # Memory per core
+#SBATCH --mem-per-cpu=8G                   # Memory per core
 #SBATCH -o hostname_%j.out                 # File to which STDOUT will be written, including job ID (%j)
 #SBATCH -e hostname_%j.err                 # File to which STDERR will be written, including job ID (%j)
                                            # You can change the filenames given with -o and -e to any filenames you'd like
