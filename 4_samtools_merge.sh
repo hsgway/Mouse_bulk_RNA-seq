4f441ade65bdfb37ad14d67e217434ba19fa77cb
hsgway <101917942+hsgway@users.noreply.github.com>
2024-01-08 23:55:53 -0500

    Update 4_samtools_merge.sh

--- 4_samtools_merge.sh
+++ 4_samtools_merge.sh
@@ -2,19 +2,19 @@
 #SBATCH -c 8                               # Request eight core
 #SBATCH -t 0-12:00                         # Runtime in D-HH:MM format
 #SBATCH -p priority                        # Partition to run in
-#SBATCH --mem-per-cpu=16G                 # Memory per core
+#SBATCH --mem-per-cpu=16G                  # Memory per core
 #SBATCH -o hostname_%j.out                 # File to which STDOUT will be written, including job ID (%j)
 #SBATCH -e hostname_%j.err                 # File to which STDERR will be written, including job ID (%j)
                                            # You can change the filenames given with -o and -e to any filenames you'd like
-#SBATCH --mail-user=yhsgw.ra@gmail.com
+#SBATCH --mail-user=xxx@bwh.harvard.edu
 #SBATCH --mail-type=ALL
 
 # load module
 module load gcc/6.2.0 samtools/1.10
 
 # directory
-dir="/n/scratch3/users/s/sk608/MappingResults/20210519"
-outdir="/n/scratch3/users/s/sk608/MappingResults/20210519_merge"
+dir="/n/scratch/users/s/<your ID>/MappingResults"
+outdir="/n/scratch/users/s/<your ID>/MappingResults_merge"
 
 mkdir $outdir
 
