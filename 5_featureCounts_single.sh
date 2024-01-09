21737cb36aed70583a4edea47408b67fbad7c572
hsgway <101917942+hsgway@users.noreply.github.com>
2024-01-08 23:58:53 -0500

    Update 5_featureCounts_single.sh

--- 5_featureCounts_single.sh
+++ 5_featureCounts_single.sh
@@ -2,22 +2,21 @@
 #SBATCH -c 8                               # Request eight core
 #SBATCH -t 0-02:00                         # Runtime in D-HH:MM format
 #SBATCH -p priority                        # Partition to run in
-#SBATCH --mem-per-cpu=16G                 # Memory per core
+#SBATCH --mem-per-cpu=8G                   # Memory per core
 #SBATCH -o hostname_%j.out                 # File to which STDOUT will be written, including job ID (%j)
 #SBATCH -e hostname_%j.err                 # File to which STDERR will be written, including job ID (%j)
                                            # You can change the filenames given with -o and -e to any filenames you'd like
-#SBATCH --mail-user=yhsgw.ra@gmail.com
+#SBATCH --mail-user=xxx@bwh.harvard.edu
 #SBATCH --mail-type=ALL
 
 # load module
 module load gcc/6.2.0 subread/2.0.0
 
 # directory and file
-bamdir="/n/scratch3/users/s/sk608/MappingResults/20210519_merge"
-outdir="/n/scratch3/users/s/sk608/Counts/20210519"
-gtf="/home/sk608/mm10_references/gencode.vM25.annotation.gtf"
+bamdir="/n/scratch3/users/<first character of your ID>/<your ID>/MappingResults"
+outdir="/n/scratch3/users/<first character of your ID>/<your ID>/Counts"
+gtf="/home/<your ID>/mm10_references/gencode.vM25.annotation.gtf"
 
-mkdir /n/scratch3/users/s/sk608/Counts
 mkdir $outdir
 
 # go to the directory where your merged BAM files locate
